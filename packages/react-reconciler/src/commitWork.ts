790579f74f0d6a4eb81459bc3ebe6747015d743d
huajiao <3080680474@qq.com>
2024-06-02 01:49:10 +0800

    feat: commit -> mutation阶段

--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -1,5 +1,7 @@
-import { FiberNode } from './fiber';
-import { MutationMask, NoFlags } from './fiberFlags';
+import { Container, appendChildToContainer } from 'hostConfig';
+import { FiberNode, FiberRootNode } from './fiber';
+import { MutationMask, NoFlags, Placement } from './fiberFlags';
+import { HostComponent, HostRoot, HostText } from './workTags';
 
 let nextEffect: FiberNode | null = null;
 
@@ -19,7 +21,7 @@ export const commitMutationEffect = (finishedWork: FiberNode) => {
       // 遍历到叶子节点或者当前fiebr树不存在subtreeFlags，此时处理fiber树本身的flags
       // 向上遍历
       up: while (nextEffect !== null) {
-        commitMutationeffectsOnFiber(nextEffect);
+        commitMutationEffectsOnFiber(nextEffect);
         // 遍历兄弟节点
         const sibling: FiberNode | null = nextEffect.sibling;
         if (sibling !== null) {
@@ -37,7 +39,90 @@ export const commitMutationEffect = (finishedWork: FiberNode) => {
  * 处理fiber存在flags的情况
  * @param nextEffect
  */
-const commitMutationeffectsOnFiber = (nextEffect: FiberNode) => {
+const commitMutationEffectsOnFiber = (finishedWork: FiberNode) => {
   // 1. 获取fiber的flags
   // 2. 判断不同的更新操作,PlaceMent、Update、ChildDeletion，随后清除对应的flags
+  const flags = finishedWork.flags;
+
+  if ((flags & Placement) !== NoFlags) {
+    commitPlacement(finishedWork);
+    // 执行完placement后去除fiber上对应的palcement  flags
+    finishedWork.flags &= ~Placement;
+  }
+
+  // flags update
+
+  // flags ChildDeletion
 };
+
+/**
+ * 执行placement操作
+ * @param finishedWork
+ */
+const commitPlacement = (finishedWork: FiberNode) => {
+  // parent DOM
+  // finishedWork -> DOM
+
+  if (__DEV__) {
+    console.warn('执行Plaecment操作', finishedWork);
+  }
+
+  // 获取父级节点的DOM
+  const hostParent = getHostParent(finishedWork);
+
+  // 找到finishedWork的DOM并append到hostParent中
+  appendPalcementNodeIntoContainer(finishedWork, hostParent);
+};
+
+/**
+ * 获取父级节点
+ * @param fiber
+ * @returns
+ */
+function getHostParent(fiber: FiberNode) {
+  let parent = fiber.return;
+
+  while (parent) {
+    const parentTag = parent.tag;
+
+    // 如果父级节点是hostComponent
+    if (parentTag === HostComponent) {
+      return parent.stateNode as Container;
+    }
+    if (parentTag === HostRoot) {
+      // 根节点的DOM存储在stateNode的container里
+      return (parent.stateNode as FiberRootNode).container;
+    }
+
+    parent = parent.return;
+  }
+
+  if (__DEV__) {
+    console.warn('未找到父节点');
+  }
+}
+
+/**
+ * 将节点插入到父节点中
+ * @param finishedWork
+ * @param hostParent
+ * @returns
+ */
+function appendPalcementNodeIntoContainer(finishedWork: FiberNode, hostParent: Container) {
+  if (finishedWork.tag === HostComponent || finishedWork.tag === HostText) {
+    appendChildToContainer(finishedWork.stateNode, hostParent);
+    return;
+  }
+
+  let child = finishedWork.child;
+  // 向下递归遍历，获取到tag是HostComponent或HostText的fiber，将其append到hostParent中
+  if (child !== null) {
+    appendChildToContainer(child, hostParent);
+    let sibling = child.sibling;
+
+    while (sibling !== null) {
+      appendChildToContainer(sibling, HostComponent);
+      sibling = sibling.sibling;
+    }
+  }
+}
